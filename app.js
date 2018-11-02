1a145f438fa779d60e1d6a7f0dbf215c2e7482b7
Jordan Michele <jordanmichele32@gmail.com>
2018-11-02 13:31:04 -0400

    added views directory and ejs

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.render("home.ejs");
+})
+app.get('/about', (req, res) => res.json({Success: 'Welcome to the about us page'}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
