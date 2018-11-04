3f0903083c6c83b2713fe1b52bc73f6d7d7fd93a
Jordan Michele <jordanmichele32@gmail.com>
2018-11-04 18:25:21 -0500

    added public folder

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
     res.render("home.ejs");
 })
+
+app.get('/favoriteteam/:favteam', (req, res) => {
+    let favteam = req.params.favteam;
+    res.render("love.ejs", {favteam: favteam});
+});
+
 app.get('/about', (req, res) => res.json({Success: 'Welcome to the about us page'}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
